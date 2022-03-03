d0de8ef345e065f29b2f32e76965535075d7277c
POZ <antoivans@gmail.com>
2022-03-03 23:10:22 +0300

    Solution of the third lab work (FoAaP)

---
+++ semester2/FoAaP/lab3/Task6/Task6/Task6.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include "geometry.h"
+
+using namespace std;
+
+void enter_number_IR(long double& n) {
+	while (!(cin >> n) || (cin.peek() != '\n')) {
+		cin.clear();
+		while (cin.get() != '\n');
+		cout << "Введено некорректное значение! Повторите ввод!!!" << endl;
+	}
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	cout << "Введите кооринаты центра круга!" << endl;
+	long double x, y;
+	cout << "Введите координату x центра: ";
+	enter_number_IR(x);
+	cout << "Введите координату y центра: ";
+	enter_number_IR(y);
+
+	cout << "Введите радиус круга: ";
+	long double radius;
+	enter_number_IR(radius);
+
+	Point center = Point(x, y);
+	Circle circle = Circle(center, radius);
+
+	circle.info();
+
+	cout << "\nПлощадь круга: " << circle.get_area() << endl;
+
+	cout << "\nВведите кооринаты точки 1 сектора!" << endl;
+	long double x1, y1;
+	cout << "Введите координату x точки 1 сектора: ";
+	enter_number_IR(x1);
+	cout << "Введите координату y точки 1 сектора: ";
+	enter_number_IR(y1);
+
+	cout << "\nВведите кооринаты точки 2 сектора!" << endl;
+	long double x2, y2;
+	cout << "Введите координату x точки 2 сектора: ";
+	enter_number_IR(x2);
+	cout << "Введите координату y точки 2 сектора: ";
+	enter_number_IR(y2);
+
+	try {
+		Point p1 = Point(x1, y1);
+		Point p2 = Point(x2, y2);
+
+		Sector sector = Sector(circle, p1, p2);
+
+		cout << "Площадь вектора: " << sector.get_area() << endl;
+	}
+	catch (std::invalid_argument& ex) {
+		cerr << "\n\n" << ex.what() << endl;
+	}
+
+	return 0;
+}
---
+++ semester2/FoAaP/lab3/Task6/Task6/geometry.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+
+class Point {
+	long double x;
+	long double y;
+public:
+	Point(long double x, long double y);
+	
+	long double get_x();
+	long double get_y();
+};
+
+class Vector {
+	long double x;
+	long double y;
+public:
+	Vector(Point p1, Point p2);
+	long double scalar_product(Vector v2);
+	static Vector vector_sum(Vector v1, Vector v2);
+	void multiply_by_scalar(long double scalar);
+	long double get_lenght();
+
+	long double get_x();
+	long double get_y();
+	void info();
+};
+
+class Circle {
+	Point center;
+	long double radius;
+public:
+	Circle(Point Center, double R);
+	long double get_area();
+
+	Point get_center();
+	long double get_radius();
+	void info();
+};
+
+class Sector: public Circle {
+	long double radius;
+	Point p1;
+	Point p2;
+	Vector rad_vector1;
+	Vector rad_vector2;
+public:
+	Sector(Circle circle, Point P1, Point P2);
+	long double get_area();
+};

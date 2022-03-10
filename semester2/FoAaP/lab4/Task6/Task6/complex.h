49071a09eff44319fa6ac8e38ccc8db2447f4090
POZ <antoivans@gmail.com>
2022-03-10 03:37:39 +0300

    Solution of the fourth lab work (FoAaP)

---
+++ semester2/FoAaP/lab4/Task6/Task6/complex.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+class complex {
+private:
+	long double Re;
+	long double Im;
+
+public:
+	complex(long double Re, long double Im);
+	long double get_Re();
+	long double get_Im();
+
+	complex operator - (complex second);
+	complex operator / (complex second);
+};

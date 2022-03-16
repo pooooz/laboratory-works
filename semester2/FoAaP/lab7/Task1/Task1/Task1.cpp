2757f4b80277ce77ee4c53c4f33462be3cc72cf2
POZ <antoivans@gmail.com>
2022-03-17 01:17:43 +0300

    Solution of the seventh lab work (FoAaP)

---
+++ semester2/FoAaP/lab7/Task1/Task1/Task1.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <vector>
+#include <iterator>
+
+using namespace std;
+
+void input_vector_items(vector<double> &sequence) {
+	cout << "Введите последовательность дробных чисел (конец ввода - Ctrl+Z)\n";
+
+	istream_iterator<double> cin_iterator(cin);
+	istream_iterator<double> cin_end;
+
+	while (cin_iterator != cin_end) {
+		sequence.push_back(*cin_iterator);
+		cin_iterator++;
+	}
+
+	if (sequence.begin() == sequence.end()) {
+		throw "Вы не ввели ни одного элемента последовательности!";
+	}
+}
+
+void print_vector_items(const vector<double> sequence) {
+	vector<double>::const_iterator sequence_iterator = sequence.cbegin();
+	cout << "Ваша последовательность: ";
+	while (sequence_iterator != sequence.end()) {
+		cout << *sequence_iterator << " ";
+		sequence_iterator++;
+	}
+	cout << endl;
+	cout << "\n";
+}
+
+// The sum of the elements of the sequence from the beginning to the last positive element (inclusive)
+double task_6(vector<double> sequence) {
+	double sum = 0;
+	vector<double>::reverse_iterator last_positive = sequence.rbegin();
+	
+	bool is_all_negative = true;
+	for (auto iter = sequence.begin(); iter != sequence.end(); iter++) {
+		if (*iter > 0) 
+			is_all_negative = false;
+	}
+
+	if (is_all_negative) {
+		throw "Последовательность не содержит положительных элементов";
+	}
+
+	while (*last_positive <= 0) {
+		last_positive++;
+	}
+
+	while (last_positive != sequence.rend()) {
+		sum += *last_positive;
+		last_positive++;
+	}
+
+	return sum;
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	try {
+		vector<double> sequence;
+
+		input_vector_items(sequence);
+		print_vector_items(sequence);
+
+		cout << "Искомая сумма элементов: " << task_6(sequence) << endl;
+	}
+	catch (const char* error) {
+		cerr << error << endl;
+	}
+
+	return 0;
+}
---
+++ semester2/FoAaP/lab7/Task2/Task2/Task2.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <vector>
+#include <iterator>
+
+using namespace std;
+
+void insertion_sort_ascending(vector<double>& sequence) {
+	for (auto i = sequence.begin() + 1; i != sequence.end(); i++)
+		for (auto j = i; j != sequence.begin() && *j < *(j - 1); j--)
+			iter_swap(j - 1, j);
+}
+
+void insertion_sort_descending(vector<double>& sequence) {
+	for (auto i = sequence.begin() + 1; i != sequence.end(); i++)
+		for (auto j = i; j != sequence.begin() && *j > *(j - 1); j--)
+			iter_swap(j - 1, j);
+}
+
+void input_vector_items(vector<double>& sequence) {
+	cout << "Введите последовательность дробных чисел (конец ввода - Ctrl+Z)\n";
+
+	istream_iterator<double> cin_iterator(cin);
+	istream_iterator<double> cin_end;
+
+	while (cin_iterator != cin_end) {
+		sequence.push_back(*cin_iterator);
+		cin_iterator++;
+	}
+
+	if (sequence.begin() == sequence.end()) {
+		throw "Вы не ввели ни одного элемента последовательности!";
+	}
+}
+
+void print_vector_items(const vector<double> sequence) {
+	vector<double>::const_iterator sequence_iterator = sequence.cbegin();
+	cout << "Ваша последовательность: ";
+	while (sequence_iterator != sequence.end()) {
+		cout << *sequence_iterator << " ";
+		sequence_iterator++;
+	}
+	cout << endl;
+	cout << "\n";
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	try {
+		vector<double> sequence;
+
+		input_vector_items(sequence);
+		print_vector_items(sequence);
+
+		cout << "Сортировка вставками по возрастанию" << endl;
+		insertion_sort_ascending(sequence);
+		print_vector_items(sequence);
+
+		cout << "Сортировка вставками по убыванию" << endl;
+		insertion_sort_descending(sequence);
+		print_vector_items(sequence);
+	}
+	catch (const char* error) {
+		cerr << error << endl;
+	}
+
+	return 0;
+}

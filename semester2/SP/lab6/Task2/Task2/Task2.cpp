70ffcb270550c688d2de82896e12e8e202309586
POZ <antoivans@gmail.com>
2022-06-21 21:25:47 +0300

    Solution of the sixth lab work (SP)

---
+++ semester2/SP/lab6/Task2/Task2/Task2.cpp
@@ -0,0 +1,91 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+template <typename n>
+void enter_number(n& x) {
+	while (!(cin >> x) || (cin.peek() != '\n')) {
+		cin.clear();
+		while (cin.get() != '\n');
+		cout << "Введено некорректное значение! Повторите ввод!!!" << endl;
+	}
+}
+
+class ArrayFingElementChallenge {
+	int length = 0;
+	vector<double> arr;
+
+	void print_array() {
+		cout << "Ваш массив: ";
+		for (auto iter = this->arr.begin(); iter != this->arr.end(); iter++) {
+			cout << *iter << ' ';
+		}
+		cout << endl;
+	}
+
+	void print_rest_of_array() {
+		double elem;
+		cout << "Введите элемент для поиска: ";
+		enter_number(elem);
+
+		auto iter = find(this->arr.begin(), this->arr.end(), elem);
+		cout << '\n';
+		if (iter != this->arr.end()) {
+			iter++;
+			if (iter != this->arr.end()) {
+				cout << "Элемент найден" << endl;
+				cout << "Оставшаяся часть массива: ";
+				for (iter; iter != this->arr.end(); iter++) {
+					cout << *iter << ' ';
+				}
+			}
+			else {
+				cout << "Элемент является последним элементом массива, за ним ничего нет" << endl;
+			}
+		}
+		else {
+			cout << "Элемент не найден" << endl;
+		}
+	}
+
+public:
+	ArrayFingElementChallenge(int length) {
+		try {
+			if (length < 1) {
+				throw invalid_argument("Длина массива не может быть меньше, чем 1");
+			}
+
+			this->length = length;
+
+			for (int i = 0; i < length; i++) {
+				double tmp;
+				cout << "Введите элемент " << i + 1 << " массива: ";
+				enter_number(tmp);
+				this->arr.push_back(tmp);
+			}
+			cout << endl;
+
+			sort(this->arr.begin(), this->arr.end());
+
+			this->print_array();
+			this->print_rest_of_array();
+		}
+		catch (invalid_argument& error) {
+			cerr << error.what() << endl;
+		}
+
+	}
+};
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	cout << "Введите количество элементов массива: ";
+	int n; 
+	enter_number(n);
+
+	ArrayFingElementChallenge resolve(n);
+
+	return 0;
+}
---
+++ semester2/SP/lab6/Task3/Task3/Task3.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <random>
+#include <ctime>
+
+using namespace std;
+
+default_random_engine randomEngine(time(NULL));
+
+int get_random_number(int min, int max) {
+	uniform_int_distribution<int> randomNum(min, max);
+	return randomNum(randomEngine);
+}
+
+class DayWithWeather {
+	bool isPresipitation;
+	int temperature;
+public:
+	DayWithWeather(bool isPresipitation, int temperature) {
+		this->isPresipitation = isPresipitation;
+		this->temperature = temperature;
+	}
+
+	bool get_presipitation() {
+		return this->isPresipitation;
+	}
+
+	int get_temperature() {
+		return this->temperature;
+	}
+};
+
+class WeatherChallenge {
+	int days_count = 31;
+	int days_without_precipitation = 0;
+	int rainy_days = 0;
+	int snowy_days = 0;
+	vector<DayWithWeather> data_array;
+
+	void print_data() {
+		cout << "\nВ этот день наблюдались осадки - 1" << endl;
+		cout << "В этот день не наблюдалось осадков - 0" << endl;
+		cout << '\n';
+		for (int i = 0; i < this->days_count; i++) {
+			cout << "День " << setw(2) << setfill('0') << i + 1 << endl;
+			cout << "Были ли осадки?: " << setw(1);
+			this->data_array[i].get_presipitation() ? cout << "Да" << endl : cout << "Нет" << endl;
+			cout << "Температура: " << this->data_array[i].get_temperature();
+			cout << '\n' << endl;
+		}
+		cout << "Дней без осадков: " << this->days_without_precipitation << endl;
+		cout << "Количество дней, когда шёл снег: " << this->snowy_days << endl;
+		cout << "Количество дней, когда шёл дождь: " << this->rainy_days << endl;
+	}
+public:
+	WeatherChallenge() {
+		for (int i = 0; i < this->days_count; i++) {
+			bool isPresipitation = bool(get_random_number(0, 1));
+			int temperature = get_random_number(-20, 5);
+
+			if (isPresipitation) {
+				if (temperature >= 0) {
+					this->rainy_days++;
+				}
+				else {
+					this->snowy_days++;
+				}
+			}
+			else {
+				this->days_without_precipitation++;
+			}
+
+			this->data_array.push_back(DayWithWeather(isPresipitation, temperature));
+		}
+		print_data();
+	}
+};
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	WeatherChallenge();
+
+	return 0;
+}

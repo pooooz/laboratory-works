0f617092801bd13add42488c51889b027d351b61
POZ <antoivans@gmail.com>
2022-06-23 01:39:48 +0300

    Solution of the ninth and tenth lab work (SP)

---
+++ semester2/SP/lab9-10/Task1/Task1/Flat.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include "Rooms.h"
+
+using namespace std;
+
+class Flat {
+	vector<Room*> rooms;
+	double price = 0;
+	double area = 0;
+public:
+	Flat(vector<Room*> rooms);
+	void print_info();
+	void sort();
+	void find_room_by_area(double area);
+
+	Flat(string file_name);
+};
+
---
+++ semester2/SP/lab9-10/Task1/Task1/Rooms.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+#include <string>
+#include <iostream>
+
+// Предположим, что у каждой комнаты будет своя цена, которая зависит от типа этой комнаты
+
+using namespace std;
+
+class Room {
+protected:
+	string name;
+	double area;
+	double price;
+	virtual void count_price() = 0;
+public:
+	Room(string name, double area);
+	double get_price();
+	double get_area();
+	void print_info();
+};
+
+class Bathroom : public Room {
+public:
+	Bathroom(string name, double area);
+
+	void count_price();
+};
+
+class Kitchen : public Room {
+public:
+	Kitchen(string name, double area);
+
+	void count_price();
+};
+
+
+class RegularRoom : public Room {
+public:
+	RegularRoom(string name, double area);
+
+	void count_price();
+};

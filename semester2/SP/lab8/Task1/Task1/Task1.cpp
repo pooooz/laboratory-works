3842866e1165713393f38125bd3bf7dafac8a830
POZ <antoivans@gmail.com>
2022-06-22 22:49:32 +0300

    Solution of the eighth lab work (SP)

---
+++ semester2/SP/lab8/Task1/Task1/Task1.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+bool isalpha_rus(char c) {
+    return c >= 'а' && c <= 'я' || c >= 'А' && c <= 'Я'
+        || c == 'ё' || c == 'Ё';
+}
+
+class FileTypedWithLetters {
+    void resolve(string file_name) {
+        ifstream file(file_name);
+        if (file.is_open()) {
+            int count = 0;
+            while (!file.eof()) {
+                char tmp_char;
+                file >> tmp_char;
+
+                if (!isalpha(tmp_char) && !isalpha_rus(tmp_char)) {
+                    throw invalid_argument("\nФайл должен состоять только из букв");
+                } 
+            }
+            file.close();
+        }
+        else {
+            throw invalid_argument("\nОшибка чтения файла");
+        }
+
+        file.open(file_name);
+        if (file.is_open()) {
+            int count = 0;
+            while (!file.eof()) {
+                char tmp_char;
+                file >> tmp_char;
+
+                if (count % 2 == 0) {
+                    cout << tmp_char;
+                }
+                count++;
+            }
+            cout << endl;
+            file.close();
+        }
+        else {
+            throw invalid_argument("\nОшибка чтения файла");
+        }
+    }
+
+public:
+    FileTypedWithLetters() {
+        try {
+            this->resolve("data.txt");
+        }
+        catch (invalid_argument& error) {
+            cerr << error.what() << endl;
+        }
+
+    }
+};
+
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    FileTypedWithLetters resolve = FileTypedWithLetters();
+
+    return 0;
+}
---
+++ semester2/SP/lab8/Task3/Task3/Task3.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class LineByLineReading {
+	int more_than_thirty_characters_lines = 0;
+	int more_than_three_spaces_lines = 0;
+	int lines_that_starts_whith_T = 0;
+
+    bool is_more_than_three_spaces(string str) {
+        int spaces_count = 0;
+
+        auto iter = str.begin();
+        while (iter != str.end() && spaces_count <= 3) {
+            if (*iter == ' ') {
+                spaces_count++;
+            }
+            iter++;
+        }
+        return spaces_count > 3;
+    }
+
+    bool is_more_than_thirty_characters(string str) {
+        return str.size() > 30;
+    }
+
+	void resolve(string file_name) {
+        ifstream file(file_name);
+        if (file.is_open()) {
+            int count = 0;
+            while (!file.eof()) {
+                string tmp_line;
+                getline(file, tmp_line);
+                if (tmp_line[0] == 'T' || tmp_line[0] == 'Т') {
+                    this->lines_that_starts_whith_T++;
+                }
+                if (this->is_more_than_three_spaces(tmp_line)) {
+                    this->more_than_three_spaces_lines++;
+                }
+                if (this->is_more_than_thirty_characters(tmp_line)) {
+                    this->more_than_thirty_characters_lines++;
+                }
+            }
+            file.close();
+
+            cout << "Количество строк, которые начинаются c \"T\": " << this->lines_that_starts_whith_T << endl;
+            cout << "Количество строк, в которых больше, чем 3 пробела: " << this->more_than_three_spaces_lines << endl;
+            cout << "количество строк, в которых больше, чем 30 символов: " << this->more_than_thirty_characters_lines << endl;
+        }
+        else {
+            throw invalid_argument("\nОшибка чтения файла");
+        }
+	}
+public:
+	LineByLineReading() {
+        try {
+            this->resolve("data.txt");
+        }
+        catch (invalid_argument& error) {
+            cerr << error.what() << endl;
+        }
+	}
+};
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+    LineByLineReading resolve = LineByLineReading();
+
+	return 0;
+}
